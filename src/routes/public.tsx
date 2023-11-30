55efd69a26b7190fd19432b27eb01ca910d2ea6d
Monica <a01707439@itesm.mx>
2023-11-30 15:28:34 +0100

    [FRONT] Route test

--- src/routes/public.tsx
+++ src/routes/public.tsx
@@ -1,8 +1,6 @@
 import { lazy } from "react";
 import { IRoute } from "../types";
 
-const Catalogue = lazy(async () => import("../pages/Catalogue"));
-
 const Homepage = lazy(async () => import("../pages/Homepage"));
 
 const Instructions = lazy(async () => import("../pages/Instructions"));
